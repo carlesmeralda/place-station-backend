7c8185b4db4f35ed4aadb93285751177b031e245
Carl Esmeralda <zxcarlesme@gmail.com>
2021-09-11 18:36:11 +0800

    added PORT env

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 mongoose
   .connect(uri)
   .then(() => {
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
   })
   .catch(err => {
     console.log(err)
